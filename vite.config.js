95ccc6247d715f2d3de21724a9ce487f1faff2fc
aviva997 <avivamalako997@gmail.com>
2023-09-28 09:32:31 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import nodeResolve from '@rollup/plugin-node-resolve'; // Add this line
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build:{
-    outDir: "build"
-  }
-})
-
-
+  plugins: [react(), nodeResolve()], // Add nodeResolve() here
+  build: {
+    outDir: 'build',
+  },
+});
